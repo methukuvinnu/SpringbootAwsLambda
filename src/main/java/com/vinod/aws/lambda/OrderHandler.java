b80bfd7e3c61d12d0bf67b775258611e74f0032e
vinod kumar methuku <methukuvinodkumar@gmail.com>
2023-06-13 13:52:09 -0500

    first commit

---
+++ src/main/java/com/vinod/aws/lambda/OrderHandler.java
@@ -0,0 +1,6 @@
+package com.vinod.aws.lambda;
+
+import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
+
+public class OrderHandler extends SpringBootRequestHandler<String,Object> {
+}
